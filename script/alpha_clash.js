0d20d60282848a7998b05507a3069d8ee83cbd58
Mostafiz Badhon <badhonhossain957@gmail.com>
2024-09-26 20:42:48 +0600

    all work done

--- script/alpha_clash.js
+++ script/alpha_clash.js
@@ -6,6 +6,42 @@
 //   showPlayground.classList.remove("hidden");
 // }
 
+function handleKeyboard(event) {
+  const playerPresed = event.key;
+
+  // escape
+  if (playerPresed === "Escape") {
+    gameOver();
+  }
+
+  // get expected to press
+  const currentAlphabetElement = document.getElementById("current_alphabet");
+  const current_alphabet = currentAlphabetElement.innerText;
+  const expectedAlphabet = current_alphabet.toLocaleLowerCase();
+
+  // check matched or not
+  if (playerPresed === expectedAlphabet) {
+    // Update score
+    const currentScore = scoreUpdate("current_score");
+    const updateScore = currentScore + 1;
+    setNewScore("current_score", updateScore);
+
+    removeBGcolor(expectedAlphabet);
+    continueGame();
+  } else {
+    // Update life
+    const currentLife = scoreUpdate("current_life");
+    const updateLife = currentLife - 1;
+    setNewScore("current_life", updateLife);
+
+    if (updateLife === 0) {
+      gameOver();
+    }
+  }
+}
+// capture keyboard key press
+document.addEventListener("keyup", handleKeyboard);
+
 function continueGame() {
   const alphabet = getARandomAlphabet();
   console.log(alphabet);
@@ -16,6 +52,24 @@ function continueGame() {
 
 function play() {
   hiddenElement("home_screen");
+  hiddenElement("final-score");
   showElement("play_ground");
+
+  // reset score And Life
+  setNewScore("current_life", 5);
+  setNewScore("current_score", 0);
+
   continueGame();
+}
+function gameOver() {
+  hiddenElement("play_ground");
+  showElement("final-score");
+
+  // set the final score
+  const lastScore = scoreUpdate("current_score");
+  setNewScore("last_score", lastScore);
+
+  // clear color
+  const currentAlphabet = getElementText("current_alphabet");
+  removeBGcolor(currentAlphabet);
 }
--- script/utility.js
+++ script/utility.js
@@ -10,6 +10,31 @@ function addBackgroundColor(elementID) {
   const element = document.getElementById(elementID);
   element.classList.add("bg-orange-400");
 }
+function removeBGcolor(elementID) {
+  const element = document.getElementById(elementID);
+  element.classList.remove("bg-orange-400");
+}
+
+// Score update function
+
+function scoreUpdate(element) {
+  const currentScoreElement = document.getElementById(element);
+  const currentScoreText = currentScoreElement.innerText;
+  const currentScore = parseInt(currentScoreText);
+  return currentScore;
+}
+function setNewScore(elementID, Value) {
+  const element = document.getElementById(elementID);
+  element.innerText = Value;
+}
+function getElementText(elementID) {
+  const element = document.getElementById(elementID);
+  const text = element.innerText;
+  return text;
+}
+
+// Life Update
+function lifeUpdate(element) {}
 
 function getARandomAlphabet() {
   const alphabetString = "abcdefghijklmnopqrstuvwxyz";
